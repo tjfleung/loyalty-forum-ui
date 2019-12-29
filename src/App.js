e2458984433401a449a821189d6cecaf455545db
Terrence Leung <tjfleung@live.ca>
2019-12-28 21:30:56 -0500

    Step 3: Connect to back-end

--- src/App.js
+++ src/App.js
@@ -1,15 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            userName: '',
+            location: '',
+            message: '',
+            resp: ''
+        }
+
+    }
+
+    postMessage(e) {
+        e.preventDefault();
+
+        const url = 'http://localhost:8000/message?message=' + this.state.message;
+
+        axios.get(url).then(
+            response => this.setState({resp: response.data})
+        );
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <form onSubmit={this.postMessage.bind(this)}>
+                    <label>Username:</label>
+                    <input type="text" name="userName" id="userName" value={this.state.userName} onChange={e => this.setState({userName: e.target.value})} cols="100" rows="1" />
+                    <br/>
+
+                    <label>Location:</label>
+                    <input type="text" name="location" id="location" value={this.state.location} onChange={e => this.setState({location: e.target.value})} cols="100" rows="1" />
+                    <br/>
+
+                    <label>Message:</label>
+                    <input type="text" name="message" id="message" value={this.state.message} onChange={e => this.setState({message: e.target.value})} cols="100" rows="4" />
+                    <br/>
+
+                    <button>Done</button>
+                </form>
+                <p>{this.state.resp}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
